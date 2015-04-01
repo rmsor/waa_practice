39286e015e4dc895f23e452013c141676fae18fc
Rameshor Pathak <rmsor.pth02@gmail.com>
2015-04-01 15:28:14 -0500

    new files

--- QuizServlet/src/java/web/QuizServlet.java
+++ QuizServlet/src/java/web/QuizServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import src.Quiz;
 
 /**
  *
@@ -18,69 +19,52 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class QuizServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet QuizServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet QuizServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private void genQuizPage(Quiz sessQuiz, PrintWriter out, String currQuest, boolean error, String answer) {
+
+        out.print("<html>");
+        out.print("<head>");
+        out.print("	<title>NumberQuiz</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("	<form method='post'>");
+        out.print("		<h3>Have fun with NumberQuiz!</h3>");
+        out.print("<p>Your current score is: ");
+        out.print(sessQuiz.getNumCorrect() + "</br></br>");
+        out.print("<p>Guess the next number in the sequence! ");
+        out.print(currQuest + "</p>");
+
+        out.print("<p>Your answer:<input type='text' name='txtAnswer' value='' /></p> ");
+
+        /* if incorrect, then print out error message */
+        if (error && (answer != null)) {  //REFACTOR?-- assumes answer null only when first open page
+            out.print("<p style='color:red'>Your last answer was not correct! Please try again</p> ");
         }
+        out.print("<p><input type='submit' name='btnNext' value='Next' /></p> ");
+
+        out.print("</form>");
+        out.print("</body></html>");
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private void genQuizOverPage(PrintWriter out) {
+        out.print("<html> ");
+        out.print("<head >");
+        out.print("<title>NumberQuiz is over</title> ");
+        out.print("</head> ");
+        out.print("<body> ");
+        out.print("<p style='color:red'>The number quiz is over!</p>	</body> ");
+        out.print("</html> ");
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       //genQuizPage();
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
