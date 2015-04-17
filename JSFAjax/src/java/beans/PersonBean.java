70b763b0a80c0078b65f1deb60c42005401db3ef
rmsor <rmsor.pth02@gmail.com>
2015-04-17 15:43:53 -0600

    change

--- JSFAjax/src/java/beans/PersonBean.java
+++ JSFAjax/src/java/beans/PersonBean.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import javax.enterprise.context.RequestScoped;
 
 import javax.inject.Named;
-import org.primefaces.model.map.DefaultMapModel;
-import org.primefaces.model.map.MapModel;
 
 @Named("pb")
 
@@ -33,48 +31,41 @@ public class PersonBean implements Serializable {
     private String state;
     private String msg;
     private String msgColor;
-    private MapModel emptyModel;
-    private double lat;
-    private double lng;
-    
-    private ArrayList<Person> persons=new ArrayList();
-    
+
+    private ArrayList<Person> persons = new ArrayList();
+
     public PersonBean() {
-        emptyModel = new DefaultMapModel();
-        persons.add(new Person("Ramesh","Pathak","rmsor.pth02@gmail.com","1000N 4TH ST MUM MR #66","52557","IA"));
-        persons.add(new Person("Dharma","Poudel","dharma@gmail.com","1000N 4TH ST MUM MR #62","52557","IA"));
-        persons.add(new Person("Raz","Mahato","raz@gmail.com","1000N 4TH ST MUM MR #65","52557","IA"));
+        persons.add(new Person("Ramesh", "Pathak", "rmsor.pth02@gmail.com", "1000N 4TH ST Fairfield IA", "52557", "IA"));
+        persons.add(new Person("Dharma", "Poudel", "dharma@gmail.com", "1000N 4TH ST MUM MR #62", "52557", "IA"));
+        persons.add(new Person("Raz", "Mahato", "raz@gmail.com", "1000N 4TH ST MUM MR #65", "52557", "IA"));
     }
-    
-    public void searchName(){
-        boolean found=false;
-        for(Person ps:persons){
-            if(ps.getFirstName().equalsIgnoreCase(firstName)){
+
+    public void searchName() {
+        boolean found = false;
+        for (Person ps : persons) {
+            if (ps.getFirstName().equalsIgnoreCase(firstName)) {
                 setEmail(ps.getEmail());
                 setLastName(ps.getLastName());
                 setEmail(ps.getEmail());
                 setStreetAddress(ps.getStreetAddress());
                 setZip(ps.getZip());
                 setState(ps.getState());
-                found=true;
+                found = true;
                 setMsg("User Found !!");
                 setMsgColor("green");
                 return;
             }
         }
-        if(!found){
+        if (!found) {
             setMsg("User Not Found !!!");
             setMsgColor("red");
         }
     }
-    
+
     public String getFirstName() {
 
         return firstName;
 
-    }
-    public MapModel getEmptyModel() {
-        return emptyModel;
     }
 
     public void setFirstName(String firstName) {
@@ -142,21 +133,5 @@ public class PersonBean implements Serializable {
     public void setMsgColor(String msgColor) {
         this.msgColor = msgColor;
     }
-    public double getLat() {
-        return lat;
-    }
 
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLng() {
-        return lng;
-    }
-
-    public void setLng(double lng) {
-        this.lng = lng;
-    }
-    
-    
 }
